1328d6ce925830eb04fbc497997adbc4b8227e28
idmyn <hello@davidmyno.rs>
2019-10-04 10:44:11 +0100

    change square brackets to regular parentheses to match api_keys.txt

--- rpgtodo/rpgtodo.py
+++ rpgtodo/rpgtodo.py
@@ -53,9 +53,9 @@ class FileRW:
         habitica_user_id = re.search(r"(?<=: )\S*", auth_contents[1]).group()
         habitica_api_token = re.search(r"(?<=: )\S*", auth_contents[2]).group()
         auth_dict = {
-            "todoist_api": todoist_api_token.translate({ord(c): None for c in '[]'}),
-            "habitica_user": habitica_user_id.translate({ord(c): None for c in '[]'}),
-            "habitica_api": habitica_api_token.translate({ord(c): None for c in '[]'})
+            "todoist_api": todoist_api_token.translate({ord(c): None for c in '()'}),
+            "habitica_user": habitica_user_id.translate({ord(c): None for c in '()'}),
+            "habitica_api": habitica_api_token.translate({ord(c): None for c in '()'})
         }
         # TODO: throw helpful error if user hasn't edited the api_keys.txt file
         return auth_dict
